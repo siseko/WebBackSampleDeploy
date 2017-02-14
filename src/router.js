d9c7a32454159ecdec1a59b6fd3518c5a783f9fa
vila_voco <s.nomavila@gmail.com>
2017-02-14 20:19:04 +0200

    Init commit

--- src/router.js
+++ src/router.js
@@ -3,20 +3,39 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
-import ArtistDetail from './components/artists/ArtistDetail';
-import ArtistCreate from './components/artists/ArtistCreate';
-import ArtistEdit from './components/artists/ArtistEdit';
+
+const componentRoutes = {
+  component: Home,
+  path: '/',
+  indexRoute: { component: ArtistMain },
+  childRoutes: [
+    {
+      path: 'artists/new',
+      getComponent(location, callback) {
+        System.import('./components/artists/ArtistCreate')
+          .then(module => callback(null, module.default));
+      }
+    },
+    {
+      path: 'artists/:id',
+      getComponent(location, callback) {
+        System.import('./components/artists/ArtistDetail')
+          .then(module => callback(null, module.default));
+      }
+    },
+    {
+      path: 'artists/:id/edit',
+      getComponent(location, callback) {
+        System.import('./components/artists/ArtistEdit')
+          .then(module => callback(null, module.default));
+      }
+    }
+  ]
+}
 
 const Routes = () => {
   return (
-    <Router history={hashHistory}>
-      <Route path="/" component={Home}>
-        <IndexRoute component={ArtistMain} />
-        <Route path="artists/new" component={ArtistCreate} />
-        <Route path="artists/:id" component={ArtistDetail} />
-        <Route path="artists/:id/edit" component={ArtistEdit} />
-      </Route>
-    </Router>
+    <Router history={hashHistory} routes={componentRoutes}/>
   );
 };
 
